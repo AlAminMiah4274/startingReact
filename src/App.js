940788017518c9db34ca6bb9cb23268f370eed69
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-01-06 21:30:34 +0600

    discussed core concept about react

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 5555;
-const singer = { name: 'Dr. Mahfuz', job: 'Singer' };
-const singer2 = { name: 'Eva Rahman', job: 'Singer2' };
+const singers = [
+  { name: 'Dr. Mahfuz', job: 'Singer' },
+  { name: 'Eva Rahman', job: 'Singer2' },
+  { name: 'Agun', job: 'Shopno' },
+  { name: 'Shuvro', job: 'Pathor' },
+];
 
 const styleSinger = {
   color: 'red',
@@ -11,12 +15,24 @@ const styleSinger = {
 }
 
 function App() {
-  const nayoks = ['Rubel', 'BappaRaz', 'Kuber', 'Joshim'];
+  const nayoks = ['Rubel', 'BappaDa', 'Kuber', 'Jashim', 'Riyaz', 'Anwar'];
   return (
     <div className="App">
-      <Person name={nayoks[0]} nayika='Mowshumi'></Person>
+      {
+        nayoks.map(nayok => <li>Name: {nayok}</li>)
+      }
+
+      {/* {
+        nayoks.map(nayok => <Person name={nayok}></Person>)
+      } */}
+
+      {
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
+      {/* <Person name={nayoks[0]} nayika='Mowshumi'></Person>
       <Person name={nayoks[1]} nayika='Cheka'></Person>
-      <Person name={nayoks[2]} nayika='Kopila'></Person>
+      <Person name={nayoks[2]} nayika='Kopila'></Person> */}
       <h4>New Component. YAY!</h4>
       <p>Rock mama React mama</p>
       <Friend movie='Singam' phone='01333'></Friend>
@@ -30,7 +46,7 @@ function Person(props) {
   return (
     <div className='person'>
       <h1>{props.name}</h1>
-      <p>Nayika: {props.nayika}</p>
+      <p>{props.nayika}</p>
     </div>
   );
 }
